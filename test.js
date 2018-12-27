e8477e03bdcf875a1219c4a5a5241ee798d790be
TJ Horner <me@tjhorner.com>
2018-12-26 17:42:45 -0800

    a fix

--- test.js
+++ test.js
@@ -4,3 +4,13 @@ const Drone = require('./lib/Drone')
 const drone = new Drone()
 
 drone.enable()
+
+setTimeout(() => {
+  drone.takeOff()
+  drone.forwardBackward = 254
+}, 2000)
+
+setTimeout(() => {
+  drone.forwardBackward = 128
+  drone.land()
+}, 4000)
